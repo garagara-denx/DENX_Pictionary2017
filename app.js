2c4746f80c8151e469c0101a39896f6029198b56
garagara-denx <sinfu000@gmail.com>
2017-10-15 20:18:25 +0900

    hostとclientでそれぞれ別のHTTPサーバを立ててソケット通信を行えるようにした

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var http = require('http');
 var socketio = require('socket.io');
 var fs = require('fs');
 
-// Client側のサーバーを構築
+var answerText = ""
+
+// Client側のHTTPサーバーを構築
 var clientServer = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
@@ -10,17 +12,7 @@ var clientServer = http.createServer(function(req, res) {
   res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
 }).listen(3000);
 
-var io = socketio.listen(clientServer);
-
-io.sockets.on('connection', function(socket) {
-  socket.on('client_to_server', function(data) {
-    io.sockets.emit('server_to_client', {
-      value: data.value
-    });
-  });
-});
-
-// Host側のサーバーを構築
+// Host側のHTTPサーバーを構築
 var hostServer = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
@@ -28,8 +20,15 @@ var hostServer = http.createServer(function(req, res) {
   res.end(fs.readFileSync(__dirname + '/host.html', 'utf-8'));
 }).listen(3100);
 
-var hostIO = socket.listen(hostServer);
+// それぞれのHTTPサーバにsocketを紐付け
+var clientIO = socketio.listen(clientServer);
+var hostIO = socketio.listen(hostServer);
 
-io.socket.on('connection', function(socket) {
-
-})
+// connectionイベントの中身を定義
+clientIO.sockets.on('connection', function(socket) {
+  socket.on('client_to_server', function(data) {
+    hostIO.sockets.emit('server_to_client', {
+      value: data.value
+    });
+  });
+});
